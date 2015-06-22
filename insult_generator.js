923d24d7291e768c7f6a35b87809cef55712f10a
duarte-pompeu <pnt12@live.com.pt>
2015-06-22 22:23:08 +0100

    add name serialization and deserialization use name serials for seeding random functions

--- insult_generator.js
+++ insult_generator.js
@@ -1,21 +1,47 @@
 var all_insults = "aldrabão,abécula,agarrado,analfabruto,atraso de vida,apanhado do clima,azeiteiro,alcoviteira,aselha,asno,anjinho,asqueroso,arruaceiro,artolas,|badameco,bandido,bruto,besta quadrada,barrigudo,brutamontes,borra-botas,bufo,boca de xarroco,boi,basbaque,biltre,bexigoso,bichona,bêbedo,bebedolas,batoque,banana,bardajona,badalhoca,bisbilhoteira,bandalho,bota de elástico,baldas,brochista,boneca de trapos,beata,bronco,bexigoso,betinho,bárbaro,|cunanas,camelo,chalado,camafeu,cona de sabão,cara de cu à paisana,coirão,choninhas,carroceiro,cabeçudo,cavalgadura,canalha,cretino,calhandreira,caga-tacos,cegueta,caixa de óculos,cornudo,coxo,candongueiro,careca,chupado das carochas,copinho de leite,cacique,calão,cabra,cabrão,cusca,coscuvilheira,cão,cabeça no ar,convencido,chanfrado,cagão,chato,cobardola,cavalona,chico-esperto,charlatão,caloteiro,cigano,comuna,carrancudo,corno,caceteiro,canalha,carapau de corrida,choné,cabeça de abóbora,|delambida,desenxabida,doido varrido,doidivanas,desmancha prazeres,desastrada,desengonçado,desaustinado,desbocado,|escanifobética,estafermo,embusteiro,estúpido,esqueleto vaidoso,engraxador,esgalgado,empecilho,estroina,escarumba,estouvada,estupor,espantalho,estapafúrdio,energúmeno,espalhafatoso,enjoado da trampa,|flausina,farsante,filho da puta,fufa,fersureira,falhado,foleiro,facínora,falsário,franganote,fanfarrão,fanático,fanchono,filho da mãe,|gatuno,gordalhufo,gabiru,galinha choca,galdéria,gabarola,gosma,gandulo,ganancioso,garganeira,|histérica,herege,|idiota,imbecil,incapaz,incompetente,inútil,|javardo,judeu,|lambisgóia,ladrão,lavajão,lambéconas,lambe-botas,lingrinhas,larápio,larilas,labrego,louco,lorpa,lunático,|morcão,malacueco,maluco,mariquinhas pé-de-salsa,meliante,mentiroso,malandro,malandreco,malandrim,marreco,maneta,mouco,mariconço,maricas,menino da mamã,mastronço,mostrengo,moina,meia-leca,medroso,monhé,molengão,mafioso,medricas,masoquista,mineteiro,maltrapilho,maria-vai-com-as-outras,miserável,magricela,mula,mal enjorcado,mimado,|nódoa,nulidade,nabo,nojento,não-tens-onde-cair-morto,nababo,|otário,olhos de carneiro mal morto,orelhas de abano,obcecado,ordinário,obstinado,|palerma,parvalhão,pateta,parvo,porcalhão,piroso,pirata,piolhoso,peida-gadoxa,pantomineiro,pote de banhas,pernas de alicate,pelintra,patego,panasca,paneleiro,putéfia,puta,panilas,pés de chumbo,patife,perliquiteques,palhaço,palhaçote,porco,punheteiro,preguiçoso,pacóvio,pobre de espírito,proxeneta,patinho feio,panhonhas,pintor,parasita,presunçoso,palonça,peneirenta,pobre diabo,|quadrilheira,queixinhas,quatro-olhos,|ranhoso,reles,rasca,rameira,rabeta,rafeiro,reaccionário,reaças,raquítico,|salafrário,safardana,sevandija,sacripanta,sacrista,sacana,sovina,somítico,safado,sabujo,saloio,soba,sebento,sapatona,sádico,serigaita,sarnento,snob,|tarado,trombalazanas,trapaceiro,trabeculoso,tísico,trombudo,trauliteiro,tinhoso,trique-lariques,tosco,totó,trombeiro,trouxa,tonto,traste,trinca-espinhas,troca-tintas,|unhas de fome,urso,|vaca gorda,vigarista,vândalo,vígaro,vigarista,velhaco,vendido,vagabundo,vira-casacas,vaidoso,|xé-xé,|zero à esquerda,zarolho,Zé-ninguém,|";
-var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
 // TODO: use names to seed
 // TODO: verify if random works decently
 
-function write_meaning(){
+function write_meaning()
+{
 	name = get_text().toLowerCase();
 	
 	var meaning_arr = get_meaning(name);
-	alert(name.toUpperCase() + "\n\n" + meaning_arr);
+	
+	pretty_print(meaning_arr);
+}
+
+
+function pretty_print(meaning_arr)
+{
+	serial_id = serialize_name(name);
+	deserial_name = deserialize_name(serial_id);
+	
+	out = "";
+	for(var i = 0; i < meaning_arr.length; i++){
+		out+= capitalize(meaning_arr[i]) + "\n";
+	}
+	
+	out+= "\n\n";
+	
+	out+= "SERIAL ID: " + serial_id + "\n";
+	out+= "DESERIALIZED: " + deserial_name + "\n";
+	
+	alert(out);
+}
+
+function capitalize(string) {
+	// taken from http://stackoverflow.com/a/1026087
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 
 function get_meaning(name)
 {	
-	seed_once(500);	
+	seed = serialize_name(name);
+	seed_once(seed);	
 	
 	var insults_res = [];
 	for(var i = 0; i < name.length; i++){
@@ -29,7 +55,7 @@ function get_meaning(name)
 		insults_res.push(splt[r]);
 	}
 	
-	reset_seed(500);
+	reset_seed(seed);
 	return insults_res;
 }
 
--- random.js
+++ random.js
@@ -1,8 +1,12 @@
 //~ Mostly from: http://stackoverflow.com/a/19301306
 
+var alphabet = "#abcdefghijklmnopqrstuvwxyz";
+
 var m_w = 123456789;
 var mask = 0xffffffff;
 var m_z = "NOT_INITIALIZED";
+
+var SHIFT_FACTOR = 100;
 
 // Takes any integer
 function seed_once(n)
@@ -34,4 +38,47 @@ function random_int(n1, n2)
 function reset_seed(n){
 	m_w = n;
 	m_z = 987654321;
+}
+
+function get_index(char)
+{
+	for(var i=0; i < alphabet.length; i++){
+		if (char == alphabet[i]){
+			return i;
+		}
+	}
+}
+
+function get_char(i)
+{
+	return alphabet[i];
+}
+
+function serialize_name(name)
+{
+	var serial = 0;
+	
+	for(var i = 0; i < name.length; i++){
+		char = name[i];
+		
+		serial*= SHIFT_FACTOR;
+		serial+= get_index(char);
+	}
+	
+	return serial;
+}
+
+function deserialize_name(serial){
+	name = "";
+	decomposed_serial = serial;
+	
+	while(decomposed_serial > 0){
+		ch_index = Math.round(decomposed_serial % SHIFT_FACTOR);
+		char = get_char(ch_index);
+		
+		name = char + name;
+		decomposed_serial = Math.round (decomposed_serial / SHIFT_FACTOR);
+	}
+	
+	return name;
 }
