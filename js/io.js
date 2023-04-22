6615384d4ada43fb3d801f99be4b027199c2398b
duarte-pompeu <duarte-pompeu@users.noreply.github.com>
2023-04-22 16:33:08 +0100

    format and cleanup a bit

--- js/io.js
+++ js/io.js
@@ -1,26 +1,21 @@
-/*
- * io.js
- * IO and string formatting.
- */
- 
-function pretty_print(meaning_matrix, name)
-{
+
+function pretty_print(meaning_matrix, name) {
 	serial_id = serialize_name(name);
 	deserial_name = deserialize_name(serial_id);
 
 	out = "";
-	for(var i = 0; i < meaning_matrix.length; i++){
+	for (var i = 0; i < meaning_matrix.length; i++) {
 		name_arr = meaning_matrix[i];
 
-		for(var j = 0; j < name_arr.length; j++){
+		for (var j = 0; j < name_arr.length; j++) {
 			var insult_from_letter = name_arr[j];
-			out+= capitalize(insult_from_letter) + "<br>";
+			out += capitalize(insult_from_letter) + "<br>";
 		}
 
-		out+= "<br><br>";
+		out += "<br><br>";
 	}
 
-	out+= "\n\n";
+	out += "\n\n";
 
 	var div = document.getElementById('name_meaning');
 	div.innerHTML = "<p>" + out + "</p>";
@@ -33,32 +28,29 @@ function capitalize(string) {
 	return string[0].toUpperCase() + string.slice(1);
 }
 
-function capitalize_words(str)
-{
-    var pieces = str.split(" ");
-    for ( var i = 0; i < pieces.length; i++ )
-    {
-        var j = pieces[i].charAt(0).toUpperCase();
-        pieces[i] = j + pieces[i].substr(1).toLowerCase();
-    }
-    return pieces.join(" ");
+function capitalize_words(str) {
+	var pieces = str.split(" ");
+	for (var i = 0; i < pieces.length; i++) {
+		var j = pieces[i].charAt(0).toUpperCase();
+		pieces[i] = j + pieces[i].substr(1).toLowerCase();
+	}
+	return pieces.join(" ");
 }
 
-function remove_glyphs(string){
-	
-	string = string.replace(/[áàãâ]+/g,'a');
-	string = string.replace(/[éèẽê]+/g,'e');
-	string = string.replace(/[íìĩî]+/g,'i');
-	string = string.replace(/[óòõô]+/g,'o');
-	string = string.replace(/[úùũû]+/g,'u');
+function remove_glyphs(string) {
+
+	string = string.replace(/[áàãâ]+/g, 'a');
+	string = string.replace(/[éèẽê]+/g, 'e');
+	string = string.replace(/[íìĩî]+/g, 'i');
+	string = string.replace(/[óòõô]+/g, 'o');
+	string = string.replace(/[úùũû]+/g, 'u');
 	string = string.replace(/ç/, 'c')
-	
+
 	return string;
 }
 
 
-function get_text()
-{
-  name = document.getElementsByName("inputbox")[0].value;
-  return name;
+function get_text() {
+	name = document.getElementsByName("inputbox")[0].value;
+	return name;
 }
--- js/requests.js
+++ js/requests.js
@@ -1,43 +1,40 @@
 /*
- * requests.js
  * Process GET requests.
- * 
  * Code taken from http://stackoverflow.com/a/831060
  */
- 
-function get_parameter(name){
-   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
-      return decodeURIComponent(name[1]);
+
+function get_parameter(name) {
+  if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+    return decodeURIComponent(name[1]);
 }
 
-function get_current_url(){
-	return window.location.href;
+function get_current_url() {
+  return window.location.href;
 }
 
-function load_new_meaning()
-{
-  name = document.getElementsByName("inputbox")[0].value;
+function load_new_meaning() {
+  let name = document.getElementsByName("inputbox")[0].value;
   name = name.replace(" ", "+");
-  
+
   // strip GET parameters from base URL
   url = get_current_url();
   i = url.indexOf("?");
-  
-  if(i > -1){
-	  url = url.substring(0,i);
+
+  if (i > -1) {
+    url = url.substring(0, i);
   }
-  
-  if(! (name == "" || name == "undefined")){
-	  url = url + "?" + "name=" + name;
+
+  if (!(name == "" || name == "undefined")) {
+    url = url + "?" + "name=" + name;
   }
-  
+
   window.location.href = url;
 }
 
-function replace_fb_url(){
-	url = get_current_url();
-	
-	var elem = document.getElementById("fb-button");
-	elem.setAttribute("data-href", url);
+function replace_fb_url() {
+  url = get_current_url();
+
+  var elem = document.getElementById("fb-button");
+  elem.setAttribute("data-href", url);
 }
 
